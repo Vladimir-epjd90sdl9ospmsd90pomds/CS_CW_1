ea127716598a6207be11b9c8d9ca848e8ca6f0a7
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-07-11 23:38:22 +0500

    Program is ready

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
-﻿void ReadArray(string[] resArray)
+﻿void ReadArray(string[] readingArray)
 {
-    for (int i = 0; i < resArray.Length; i++)
+    for (int i = 0; i < readingArray.Length; i++)
     {
-        Console.Write($"{resArray[i]} ");
+        Console.Write($"{readingArray[i]} ");
+    }
+}
+
+void FilterArray(string[] workArray, string[] resArray)
+{
+    int j = 0;
+    for (int i = 0; i < workArray.Length; i++)
+    {
+        if (workArray[i].Length <= 3)
+        {
+            resArray[j] = workArray[i];
+            j++;
+        }
     }
 }
 
 
-string[] resultArray = new string[] { "6", "дол", "9514", "12", "9fjh4" };
+string[] startArray = new string[] { "6", "дол", "9514", "12", "Jotf", "2A", "9Hgf4" };
+string[] resultArray = new string[startArray.Length];
 
+FilterArray(startArray, resultArray);
 ReadArray(resultArray);
