a611ecead574477afadae80381b3afbeacf76934
Vladimir-epjd90sdl9ospmsd90pomds <vololodja@yandex.ru>
2023-07-11 22:12:27 +0500

    Arrayreader is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ReadArray(string[] resArray)
+{
+    for (int i = 0; i < resArray.Length; i++)
+    {
+        Console.Write($"{resArray[i]} ");
+    }
+}
+
+
+string[] resultArray = new string[] { "6", "дол", "9514", "12", "9fjh4" };
+
+ReadArray(resultArray);
